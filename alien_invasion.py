6a6a658dcef05ece5fdbc3006646e24dc44d0ab0
7Fin7 <finley.leggett@gmail.com>
2025-04-10 15:21:19 +0200

    Created a settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@
 import sys  # Use tools in sys to exit the game when the player quits
 import pygame  # Contains functionality to make a game
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
     
@@ -11,18 +13,18 @@ class AlienInvasion:
         # Initialise the imported pygame modules
         pygame.init()
 
-        # Set the dimensions of the game window and create the display surface
-        # Surface: part of the screen where a game element can be displayed
-        self.screen = pygame.display.set_mode((1000, 600))
-
         # Set the title of the game window
         pygame.display.set_caption("Alien Invasion")
 
         # Create a Clock object to manage how fast the screen updates (frame rate)
         self.clock = pygame.time.Clock()
 
-        # Set the background colour
-        self.bg_color = (230, 230, 230)
+        self.settings = Settings()
+
+        # Set the dimensions of the game window and create the display surface
+        # Surface: part of the screen where a game element can be displayed
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -37,7 +39,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Updates the background colour
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_colour)
 
             # Update the screen with the latest drawings and changes
             pygame.display.flip()
