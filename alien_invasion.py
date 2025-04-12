58c98b777e78fa546b59686290bbb65823d57a56
7Fin7 <finley.leggett@gmail.com>
2025-04-12 20:09:44 +0200

    Add right keypress to move ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -53,6 +53,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     # Exit the game if the window is closed
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        # Move the ship to the right
+                        self.ship.rect.x += 1
 
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen."""
