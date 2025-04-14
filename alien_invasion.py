7aa1cdcebada2d76ea96c6db36704326c0ff7b4b
7Fin7 <finley.leggett@gmail.com>
2025-04-14 08:56:18 +0200

    Adding left movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -55,13 +55,17 @@ class AlienInvasion:
                     # Exit the game if the window is closed
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
+                    # Set movement flag to true
                     if event.key == pygame.K_RIGHT:
-                        # Set right movement flag to true
                         self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
                 elif event.type == pygame.KEYUP:
+                    # Set movement flag to false
                     if event.key == pygame.K_RIGHT:
-                        # Set right movement flag to false
                         self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen."""
