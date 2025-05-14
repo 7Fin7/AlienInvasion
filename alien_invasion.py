cc536d97e940ab1b841bebbe50103e08ce26edd2
7Fin7 <finley.leggett@gmail.com>
2025-05-14 11:13:56 +0100

    Moving the aliens right

--- alien_invasion.py
+++ alien_invasion.py
@@ -53,6 +53,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             # Limit the game loop to a maximum of 60 frames per second
@@ -127,6 +128,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     
     def _update_screen(self):
