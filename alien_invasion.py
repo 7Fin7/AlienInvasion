18921f888bac297dc6ccccbc75083c64f7132b1a
7Fin7 <finley.leggett@gmail.com>
2025-10-13 18:35:56 +0100

    Making larger bullets for testing

--- alien_invasion.py
+++ alien_invasion.py
@@ -157,7 +157,7 @@ class AlienInvasion:
         # Check for any bullets that have hit aliens.
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
-            self.bullets, self.aliens, True, True
+            self.bullets, self.aliens, False, True
         )
 
 
