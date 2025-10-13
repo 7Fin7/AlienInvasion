8f0f654a99e289d585c68864dff0bd3f232d5975
7Fin7 <finley.leggett@gmail.com>
2025-10-13 21:19:47 +0100

    Speeding up the bullets

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings
-        self.bullet_speed = 2.0
+        self.bullet_speed = 2.5
         self.bullet_width = 350
         self.bullet_height = 15
         self.bullet_colour = (60, 60, 60)
