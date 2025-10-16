40e1947267972107e5394d9d150324924b16d9e1
7Fin7 <finley.leggett@gmail.com>
2025-10-16 22:05:49 +0100

    Aliens that reach the bottom of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -188,6 +188,9 @@ class AlienInvasion:
             # If a collision is detected, print a message to the console.
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         # Decrement ships_left.
@@ -203,6 +206,14 @@ class AlienInvasion:
 
         # Pause.
         sleep(0.5)
+        
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen."""
