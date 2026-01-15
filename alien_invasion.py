a367f99dc993225b1cef83357a77906aedc9cfdb
Finley Leggett <finley.leggett@gmail.com>
2026-01-15 16:00:03 +0000

    Resetting the speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -166,6 +166,10 @@ class AlienInvasion:
         # collidepoint() returns True if the given point (x, y) lies within the button’s rect.
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
+
+            # Reset the game settings
+            self.settings.initialize_dynamic_settings()
+
             # Reset the game statistics
             self.stats.reset_stats()
             self.game_active = True
